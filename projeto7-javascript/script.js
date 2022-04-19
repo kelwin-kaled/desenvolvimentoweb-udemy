7f8d3f389e7f60035dd5f3ba9e2a27882f033c19
kelwin-kaled <101134615+kelwin-kaled@users.noreply.github.com>
2022-04-19 09:50:31 -0300

    update

--- projeto7-javascript/script.js
+++ projeto7-javascript/script.js
@@ -1,7 +1,28 @@
-var média = 7
-var nota = prompt('Digite a nota do aluno')
-if(nota>média){
-    document.write('Aprovado')
-}else{
-    document.write('Reprovado')
+var nome = window.prompt('Digite o seu nome')
+var altura = window.prompt('Digite sua altura em cm')
+var peso = window.prompt('Digite o seu peso em Kg')
+altura = parseFloat(altura)
+peso = parseFloat(peso)
+altura = altura/100
+var indice = peso / (altura^2)
+document.write(indice.toFixed(2))
+if(indice<16){
+
+}else if(indice>=16&&indice<16.99){
+
+}
+else if(indice>=16.99&&indice<16.99){
+
+}
+else if(indice>=16&&indice<16.99){
+
+}
+else if(indice>=16&&indice<16.99){
+
+}
+else if(indice>=16&&indice<16.99){
+
+}
+else if(indice>=16&&indice<16.99){
+
 }
