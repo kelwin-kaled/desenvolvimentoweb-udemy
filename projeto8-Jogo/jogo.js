4f1afce74d78e92d93251e6fa9e3866607b45817
kelwin-kaled <101134615+kelwin-kaled@users.noreply.github.com>
2022-07-04 13:39:36 -0300

    update

--- projeto8-Jogo/jogo.js
+++ projeto8-Jogo/jogo.js
@@ -5,6 +5,9 @@ function redimensiona(){
 }
 function geramosquito(){
     redimensiona()
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    }
     let posicaox = Math.floor(Math.random()*largura)
     let posicaoy = Math.floor(Math.random()*altura)
     let tamanho =Math.floor(Math.random()*4)
@@ -14,6 +17,8 @@ function geramosquito(){
     document.body.appendChild(mosca)
     mosca.src = 'https://images.vexels.com/media/users/3/223215/isolated/preview/e14fffc1481df5ec32df45f6d1b05dd7-ilustracao-de-mosca-domestica.png'
     mosca.className = 'mosquito' + tamanho
+    mosca.id = 'mosquito'
+    mosca.onclick = function(){pontos()}
     if(posicaox <=90 || posicaoy <=90){
         mosca.style.left = posicaox + 'px'
         mosca.style.top = posicaoy + 'px'
@@ -25,4 +30,28 @@ function geramosquito(){
     }
 
 }
-    var altura, largura
+function pontos(){
+    document.getElementById('mosquito').remove()
+    pontuacao += 1
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML = `Pontos: ${pontuacao}`
+}
+function cronometro(){
+    if(tempo==0){
+        clearInterval(intervaloM)
+        clearInterval(intervaloC)
+    }else{
+        tempo-=1
+        let croc = document.getElementById('tempos')
+        croc.innerHTML = `Tempo:${tempo}`
+    }
+}
+function IniciaJogo(){
+    setInterval(geramosquito,3000)
+    setInterval(cronometro,1000)
+}
+function EncerraJogo(){
+    clearInterval(intervaloM)
+    clearInterval(intervaloC)
+}
+   var altura, largura, pontuacao = 0 , tempo = 60, dificuldade
